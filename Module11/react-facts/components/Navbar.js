87f0bcea900595bd50374926fab41996d34a33b3
Simina <coneac@gmail.com>
2023-06-07 13:19:59 +0300

    react projects

---
+++ Module11/react-facts/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <img src="images/react-icon-small.png" alt="logo" className="nav-logo"/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
